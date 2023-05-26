3caca4fd54168d208d252d11a4ee5aecb08928dc
lutianyu.990919 <1141049271@qq.com>
2023-05-26 18:05:10 +0800

    打印日志

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+console.log(inter);
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
