639f31351dfc79094d97904056ca9c8fe670bb71
lutianyu.990919 <1141049271@qq.com>
2023-05-29 19:01:32 +0800

    feat(About.tsx): 新增monaco-editor,未完成

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as monaco from "monaco-editor";
 
 export default function About() {
-  return <div>About</div>;
+  useEffect(() => {
+    monaco.editor.create(document.getElementById("container")!, {
+      language: "javascript",
+    });
+  }, []);
+  return (
+    <div>
+      <h2>About</h2>
+      <div id="container" />
+    </div>
+  );
 }
