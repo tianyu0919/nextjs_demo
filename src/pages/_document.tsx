ce7cc941c85135e065872d945f443097ab00f307
lutianyu.990919 <1141049271@qq.com>
2023-05-31 18:58:05 +0800

    feat(editor): 未完成

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script type="module" src="../../node_modules/monaco-editor/esm/vs/editor/editor.main.js" defer />
       <body>
+        xx1
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
